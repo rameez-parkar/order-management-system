d56d13d3ed1c18a38dc6dc7ffd560fd455b0849b
Rameez Parkar <rparkar@tavisca.com>
2021-06-24 02:04:11 +0530

    completed html structure of login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { components } from './components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    components
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
